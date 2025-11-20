0c583b0a08cc4ec78ec9cc63c5b2142e8ac1701e
exitgh0st <dev.exitgh0st@gmail.com>
2025-11-21 07:19:00 +0800

    Add checking of platform

--- src/app/features/auth/guards/guest.guard.ts
+++ src/app/features/auth/guards/guest.guard.ts
@@ -1,10 +1,17 @@
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthStore } from "../services/auth-store";
-import { inject } from "@angular/core";
+import { inject, PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 export const guestGuard: CanActivateFn = () => {
     const authStore = inject(AuthStore);
     const router = inject(Router);
+
+    const platformId = inject(PLATFORM_ID);
+
+     if (!isPlatformBrowser(platformId)) {
+       return true;
+     }
 
     if (authStore.isAuthenticated()) {
         router.navigate(['/home']);
